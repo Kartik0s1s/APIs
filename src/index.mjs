cafb6365f610722e4022d0412d93f25dea7a15b5
Kartik Chaudhar <chaudharikartik532@gmail.com>
2025-10-20 23:01:40 +0530

    Created first API

---
+++ src/index.mjs
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app = express();
+
+
+app.get("/",(req, resp)=>{
+  resp.send("Our Api is ready ")
+  resp.end()
+})
+
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+    console.log("App is running on 3000")
+})
